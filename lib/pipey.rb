3dd5d6026fc7b6918dfbb0b3f2d4cd414e779453
vladholovchak <vlad.golovchak.1996@gmail.com>
2024-10-02 13:13:14 +0300

    update to ruby 3

--- lib/pipey.rb
+++ lib/pipey.rb
@@ -2,8 +2,6 @@ require 'pipey/version'
 require 'pipey/line'
 require 'pipey/chain'
 require 'pipey/steps'
-require 'pipey/steps'
-require 'pipey/extensions'
 require 'pipey/extensions'
 
 module Pipey
